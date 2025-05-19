3474fca17067049153f3134c303a6ccbe28ba4c5
kristerssparans <kristers.sparans@edu.rtu.lv>
2025-05-19 20:26:32 +0300

    Pabeigti pamati prieks projekts.py

--- projekts.py
+++ projekts.py
@@ -18,11 +18,22 @@ if lapa.status_code == 200:
         print(f"{symbol} - was found")
         jaunas_lapas_saturs = BeautifulSoup(jauna_lapa.content, "html.parser")
 
-        forecasts = jaunas_lapas_saturs.find(data_title_="Forecast")
-        print(forecasts)
+        navigation = jaunas_lapas_saturs.find(class_="navmenu").find_all("a")
+        for value in navigation:
+            if value.text == "Forecast":
+                forecast_page = "https://stockanalysis.com" + value.attrs["href"]
 
-        #paregojums = jaunas_lapas_saturs.find(class_="-mt-2 text-center text-xl font-semibold")
-        #print(paregojums.find("span").text)
+                open_forecast = requests.get(forecast_page)
+
+                if open_forecast.status_code == 200:
+                    forecast_saturs = BeautifulSoup(open_forecast.content, "html.parser")
+
+                    paregojums = forecast_saturs.find(class_="-mt-2 text-center text-xl font-semibold")
+                    print(f"Expertu viedoklis par izvēlēto akciju - {paregojums.find("span").text}")
+                    exists = True
+                
+        if exists != True:
+            print("Nav pieejams ekspertu viedoklis.")
     else:
         print("Netika atrastas akcijas.")
 
